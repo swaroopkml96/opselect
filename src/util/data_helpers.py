bc394bd9ba8d418357886396ec29db26acfca0e5
swaroop <swaroopkumar.ml@ltts.com>
2022-04-07 21:15:28 +0530

    structure of file names changed

--- src/util/data_helpers.py
+++ src/util/data_helpers.py
@@ -4,4 +4,4 @@ def get_files(images_path):
     return [os.path.join(images_path, p) for p in os.listdir(images_path)]
 
 def get_labels(file_paths):
-    return [os.path.basename(p).split('_s_')[0] for p in file_paths]
+    return [os.path.basename(p).split('_')[0] for p in file_paths]
